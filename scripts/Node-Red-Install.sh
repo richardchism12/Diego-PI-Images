8f1786e359c932df5e8a498f2a82828e929775c4
richardchism12 <35053133+richardchism12@users.noreply.github.com>
2019-10-22 21:24:39 -0500

    Update Node-Red-Install.sh

--- scripts/Node-Red-Install.sh
+++ scripts/Node-Red-Install.sh
@@ -1,2 +1,10 @@
 ssh pi@raspberrypi raspberry
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+
+If you want Node-RED to run when the Pi is turned on, or re-booted, you can enable the service to autostart by running the command:
+
+sudo systemctl enable nodered.service
+
+To disable the service, run the command:
+
+sudo systemctl disable nodered.service
